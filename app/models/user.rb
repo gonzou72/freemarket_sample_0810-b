ca4c84d5c9dbf0292177e565f6f567cbf93928b2
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-08-28 20:15:20 +0900

    Update user.rb
    validation設定。
    
    テストコード記述まで暫定で記入。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 7...128
+  validates :nickname, :email, :password, :last_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_day, presence: true
+  validates :nickname, :email, uniqueness: true
+  validates :first_name_kana, :last_name_kana, format: { with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナで入力して下さい。'}
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角で入力して下さい。'}
 end
