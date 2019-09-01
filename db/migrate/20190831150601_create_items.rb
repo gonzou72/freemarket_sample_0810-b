baeb8927ac30fe0b7ee8582a80a0f39414ecef9f
gonzou72 <s.gonzou@gmail.com>
2019-09-01 11:48:43 +0900

    Update 20190831150601_create_items.rb
    スペルミス、他

--- db/migrate/20190831150601_create_items.rb
+++ db/migrate/20190831150601_create_items.rb
@@ -1,19 +1,19 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-      t.srting      :name           ,null:false
-      t.text        :dtails         ,null:false
-      t.integer     :price          ,null:false
-      t.integer     :size           ,null:false
-      t.integer     :condition      ,null:false
-      t.integer     :shipping_fee   ,null:false
-      t.srting      :shipping_method,null:false
-      t.srting      :ship_out_area  ,null:false
-      t.srting      :ship_out_date  ,null:false
-      t.references  :user_id        ,null:false,foreign_key:true
-      t.references  :brand_id       ,null:false,foreign_key:true
-      t.references  :status_id      ,null:false,foreign_key:true
-      end
+      t.string      :name            ,null: false
+      t.text        :details         ,null: false
+      t.integer     :price           ,null: false
+      t.integer     :size            ,null: false
+      t.integer     :condition       ,null: false
+      t.integer     :shipping_fee    ,null: false
+      t.string      :shipping_method ,null: false
+      t.string      :ship_out_area   ,null: false
+      t.string      :ship_out_date   ,null: false
+      t.references  :user            ,null: false, foreign_key: true
+      t.references  :brand           ,null: false, foreign_key: true
+      t.references  :status          ,null: false, foreign_key: true
+    
       t.timestamps                   null:false
     end
   end
