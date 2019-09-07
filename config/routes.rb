af8a77763099edb774f09722150eb6b15fa2c5e0
keisuke imuta <keikeimu12@gmail.com>
2019-09-07 12:42:11 +0900

    update item register

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'news#index'
-    resources :news, only: [:index,:new,:create]
-    resources :items, only: [:new,:create]
+  root 'items#index'
+    resources :items, only: [:index,:new,:create]
 end
