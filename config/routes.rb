c3da9eb9d56340843584caf399e4c806edb17227
gonzou72 <s.gonzou@gmail.com>
2019-09-11 00:08:54 +0900

    Update routes.rb
    showアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'news#index'
     resources :news, only: [:index,:new,:create]
-    resources :items, only: [:new,:create]
+    resources :items, only: [:new,:create,:show]
 end
