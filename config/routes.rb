3f70a5ccce5083c7ef803a70fc68a1d1acac3753
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-09-17 22:27:24 +0900

    Update routes.rb
    mypages/profile表示用のルーティングと
    profile_updateのdeviseを経由する記載。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' } 
+  devise_scope :user do
+    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+  end
+
   root to: 'news#index'
     resources :news, only: [:index,:new,:create]
     resources :items, only: [:new,:create]
-    resources :mypages, only: [:index]
+    resources :mypages, only: [:index] do
+      collection do
+        get :profile
+      end
+    end
 end
