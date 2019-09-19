120974af90ec83617aec85b831ead33c6b95f4ca
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-09-19 22:08:01 +0900

    Update mypages_controller.rb
    コントローラーの記載。
    
    登録初期にaddressテーブルに入力が必須としているが、
    現状未記入でサインアップできるため、createアクションも記載。
    今後の機能が増加する予想のため、別途表記で定義する。

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,7 +1,45 @@
 class MypagesController < ApplicationController
+before_action :address_existence, only: [:identification, :identification_update]
   def index
   end
 
   def profile
+  end
+
+  def identification
+    if @address == nil
+      @address = Address.new
+      @path = identification_create_mypages_path
+    else
+      @path = identification_update_mypages_path(id: current_user.id)
+    end
+  end
+
+  def identification_create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to identification_mypages_path, notice: '本人情報を登録しました'
+    else
+      @path = identification_create_mypages_path
+      render 'mypages/identification'
+    end
+  end
+
+  def identification_update
+    if @address.update(address_params)
+      redirect_to identification_mypages_path, notice: '本人情報を更新しました'
+    else
+      @path = identification_update_mypages_path(id: current_user.id)
+      render 'mypages/identification'
+    end
+  end
+
+  private
+  def address_existence
+    @address = Address.find_by(user_id: current_user.id)
+  end
+
+  def address_params
+    params.require(:address).permit(:user_id, :postal_code, :prefecture, :city, :street, :building_name, :phone)
   end
 end
