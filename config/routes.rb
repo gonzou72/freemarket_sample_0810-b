fbbe39b6cc0f380ce5f8fd83e7a3d0d8db4afb9c
michirums <myc64sky@gmail.com>
2019-09-22 04:51:19 +0900

    step4,5,6追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     get 'step1_top', to: 'users/registrations#step1_top', as: 'step1_top'
     get 'step2_mail', to: 'users/registrations#step2_mail', as: 'step2_mail'
     get 'step3_tel', to: 'users/registrations#step3_tel', as: 'step3_tel'
+    get 'step4_address', to: 'users/registrations#step4_address', as: 'step4_address'
+    get 'step5_payment', to: 'users/registrations#step5_payment', as: 'step5_payment'
+    get 'step6_complete', to: 'users/registrations#step6_complete', as: 'step6_complete'
   end
 
   root to: 'items#index'
