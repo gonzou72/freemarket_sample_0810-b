83bd6b71743bdbdcd8c65fa2b555e0c9d8bf38a0
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-09-22 23:12:20 +0900

    Update routes.rb
    ルーティング　resoucesのみで構築の案１

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'users/registrations' } 
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
-    patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
+    namespace :users do
+      resources :registrations, only: [:index, :new] do
+        resources :phones ,only: [:new] 
+        resources :addresses, only: [:new]
+        resources :payments, only: [:new, :create]
+      end
+    end
   end
 
   root to: 'items#index'
@@ -12,16 +18,6 @@ Rails.application.routes.draw do
         get :profile, :identification
         patch :identification_update
         post :identification_create
-      end
-    end
-    resources :signup do
-      collection do
-        get 'step1_top'
-        get 'step2_mail'
-        get 'step3_tel'
-        get 'step4_address'
-        get 'step5_payment'
-        get 'step6_complete'
       end
     end
 end
