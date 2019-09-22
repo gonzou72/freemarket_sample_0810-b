69a6fbe1350e3f4bbfa8deb5e31f0c5d44070662
michirums <myc64sky@gmail.com>
2019-09-22 20:37:58 +0900

    Create signup_controller.rb

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,21 @@
+class SignupController < ApplicationController
+
+  def step1_top
+  end
+
+  def step2_mail
+  end
+
+  def step3_tel
+  end
+
+  def step4_address
+  end
+
+  def step5_payment
+  end
+
+  def step6_complete
+  end
+  
+end
