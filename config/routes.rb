a53fc92c46f75af1e94d035b696a8e2110eeff37
michirums <myc64sky@gmail.com>
2019-09-22 14:32:58 +0900

    routingをresourcesに修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' } 
   devise_scope :user do
     patch 'profile_update', to: 'users/registrations#profile_update', as: 'profile_update'
-    get 'step1_top', to: 'users/registrations#step1_top', as: 'step1_top'
-    get 'step2_mail', to: 'users/registrations#step2_mail', as: 'step2_mail'
-    get 'step3_tel', to: 'users/registrations#step3_tel', as: 'step3_tel'
-    get 'step4_address', to: 'users/registrations#step4_address', as: 'step4_address'
-    get 'step5_payment', to: 'users/registrations#step5_payment', as: 'step5_payment'
-    get 'step6_complete', to: 'users/registrations#step6_complete', as: 'step6_complete'
   end
 
   root to: 'items#index'
@@ -17,6 +11,16 @@ Rails.application.routes.draw do
         get :profile, :identification
         patch :identification_update
         post :identification_create
+      end
+    end
+    resources :signup do
+      collection do
+        get 'step1_top'
+        get 'step2_mail'
+        get 'step3_tel'
+        get 'step4_address'
+        get 'step5_payment'
+        get 'step6_complete'
       end
     end
 end
