0508904adbf5104a69bf62f288a71ebf27216e9d
michirums <myc64sky@gmail.com>
2019-09-24 22:47:53 +0900

    signupコントローラーに移管

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -69,23 +69,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
   
-  def step1_top
-  end
-
-  def step2_mail
-  end
-
-  def step3_tel
-  end
-
-  def step4_address
-  end
-
-  def step5_payment
-  end
-
-  def step6_complete
-  end
+  
   
   protected
   
