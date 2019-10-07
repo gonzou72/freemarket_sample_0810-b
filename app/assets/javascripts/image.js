54846133910bfcc9a4c9b87eb7683bd98dac67b7
keisuke imuta <keikeimu12@gmail.com>
2019-10-07 18:55:18 +0900

    update item_save(ループ回すver)

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,62 +1,17 @@
 $(function () {
-  function append_one(){
-    var append_one=`<label for="item_images_attributes_1_image">
-                      <input type="file" name="item[images_attributes][1][image]" id="item_images_attributes_1_image">
+  function append(index){
+    var append=`<label for="item_images_attributes_${index+1}_image">
+                      <input type="file" name="item[images_attributes][${index+1}][image]" id="item_images_attributes_${index+1}_image">
                       <i class="fa fa-camera camera-icon"></i>
                     </label>`
-    $("#image_field").append(append_one);
+    $("#image_field").append(append);
   };
-  $("#item_images_attributes_0_image").on("change",function() { 
-    $("#item_images_attributes_0_image").css("display","none")
-    $(".camera-icon").css("display","none")
-    append_one();
-  });
-  function append_two(){
-    var append_two=`<label for="item_images_attributes_2_image">
-                      <input type="file" name="item[images_attributes][2][image]" id="item_images_attributes_2_image">
-                      <i class="fa fa-camera camera-icon"></i>
-                    </label>`
-    $("#image_field").append(append_two);
-  };
-  $(document).on("change","#item_images_attributes_1_image",function() { 
-    $("#item_images_attributes_1_image").css("display","none")
-    $(".camera-icon").css("display","none")
-    append_two();
-  });
-  function append_three(){
-    var append_three=`<label for="item_images_attributes_3_image">
-                  <input type="file" name="item[images_attributes][3][image]" id="item_images_attributes_3_image">
-                  <i class="fa fa-camera camera-icon"></i>
-                </label>`
-    $("#image_field").append(append_three);
-  };
-    $(document).on("change","#item_images_attributes_2_image",function() { 
-    $("#item_images_attributes_2_image").css("display","none")
-    $(".camera-icon").css("display","none")
-    append_three();
-  });
-  function append_four(){
-    var append_four=`<label for="item_images_attributes_4_image">
-                  <input type="file" name="item[images_attributes][4][image]" id="item_images_attributes_4_image">
-                  <i class="fa fa-camera camera-icon"></i>
-                </label>`
-    $("#image_field").append(append_four);
-  };
-    $(document).on("change","#item_images_attributes_3_image",function() { 
-    $("#item_images_attributes_3_image").css("display","none")
-    $(".camera-icon").css("display","none")
-    append_four();
-  });
-  function append_five(){
-    var append_five=`<label for="item_images_attributes_5_image">
-                  <input type="file" name="item[images_attributes][5][image]" id="item_images_attributes_5_image">
-                  <i class="fa fa-camera camera-icon"></i>
-                </label>`
-    $("#image_field").append(append_five);
-  };
-    $(document).on("change","#item_images_attributes_4_image",function() { 
-    $("#item_images_attributes_4_image").css("display","none")
-    $(".camera-icon").css("display","none")
-    append_five();
+  i=["0","1","2","3","4","5","6","7","8"];
+  $.each(i,function(index){
+    $(document).on("change","#item_images_attributes_" + index + "_image",function() { 
+      $("#item_images_attributes_" + index + "_image").css("display","none")
+      $(".camera-icon").css("display","none")
+      append(index);
+    });
   });
 });
