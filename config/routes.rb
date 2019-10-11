886a6bafae29b94dde2be94fcb24819334a67a7d
keisuke imuta <keikeimu12@gmail.com>
2019-10-11 21:58:53 +0900

    update item_buy

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,14 @@ Rails.application.routes.draw do
     resources :buyers, only: [:new,:create]
     resources :items, only: [:new,:create,:show]
 
-  resources :mypages, only: [:index, :destroy] 
+  resources :mypages, only: [:index, :destroy]
   scope '/mypages' do
-    resources :cards, only: [:index, :new]
+    resources :cards, only: [:new, :create, :show] do
+      collection do
+        post 'pay', to: 'cards#pay'
+        post 'delete', to: 'cards#delete'
+      end
+    end
     resources :identifications, only: [:index, :create, :update]
     resources :profiles, only: [:edit]
   end
