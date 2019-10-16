dc339719dbeb2394d26b9da35fda833abc822705
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-10-16 17:24:53 +0900

    Update card_controller_spec.rb
    index　アクション削除のため

--- spec/controllers/card_controller_spec.rb
+++ spec/controllers/card_controller_spec.rb
@@ -3,27 +3,6 @@ require 'rails_helper'
 describe CardsController do
   let(:user) { create(:user) }
 
-  describe "#index" do
-    context 'log in' do
-      before do
-        login user
-        get :index
-      end
-      it 'renders the :index template' do
-        expect(response).to render_template :index
-      end
-    end
-
-    context 'not log in' do
-      before do
-        get :index
-      end
-      it 'redirects to new_user_session_path' do
-        expect(response).to redirect_to(new_user_session_path)
-      end
-    end
-  end
-
   describe "#new" do
     context 'log in' do
       before do
