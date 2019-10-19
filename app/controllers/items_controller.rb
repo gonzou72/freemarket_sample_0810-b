9e147983c5992f9667122ed6dbcdc7c0e704dfd1
michirums <myc64sky@gmail.com>
2019-10-19 20:30:47 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     @items = Item.order('id DESC').limit(4)
@@ -40,17 +41,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path, notice: "商品が削除されました"
+    else
+       redirect_to item_path(@item), alert: "削除に失敗しました"
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:name,:detail,:price,:size,:brand,:condition,:shipping_fee,:shipping_method,:ship_out_area,:ship_out_date,:category_id,images_attributes:[:image]).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
