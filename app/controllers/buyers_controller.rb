b4485c154e6f46f7d72f12f38793687a6fb496f3
keisuke imuta <keikeimu12@gmail.com>
2019-10-19 20:52:53 +0900

    update item_buy modify

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -11,13 +11,15 @@ class BuyersController < ApplicationController
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
+    @items = Item.find(params[:item_id])
   end
 
   def pay
     card = Card.where(user_id: current_user.id).first
+    @items=Item.find(1)
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-      amount: 1000,
+      amount: @items.price,
       customer: card.customer_id,
       currency: 'jpy',
     )
@@ -25,9 +27,15 @@ class BuyersController < ApplicationController
   end
 
   def done
+    @items=Item.(1)
     card = Card.where(user_id: current_user.id).last
     customer = Payjp::Customer.retrieve(card.customer_id)
     @default_card_information = customer.cards.retrieve(card.card_id)
+  end
+
+  private 
+  def item_id
+    params.require().permit(:id)
   end
 
 end
