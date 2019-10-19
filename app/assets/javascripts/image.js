ff59ee441d71c08ae7f6e43032025d71d95c656e
kojima0415 <kojima0415tsuyoshi@gmail.com>
2019-10-19 20:37:46 +0900

    Update image.js
    editページ要のjs　最初に登録されているものは隠してしまう

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -6,12 +6,28 @@ $(function () {
                     </label>`
     $("#image_field").append(append);
   };
+
   i=["0","1","2","3","4","5","6","7","8"];
+
   $.each(i,function(index){
     $(document).on("change","#item_images_attributes_" + index + "_image",function() { 
       $("#item_images_attributes_" + index + "_image").css("display","none")
       $(".camera-icon").css("display","none")
       append(index);
     });
+  });
+  $(function(){
+    for (var i = 0; i < 10; i++) {
+      if($("#item_images_attributes_" + i + "_id").length){
+        $("#item_images_attributes_" + i + "_image").css("display","none")
+        $(".camera-icon").css("display","none")
+      } else {
+        if (i == 0){
+        }else{
+        append(i);
+        }
+        break;
+      };
+    };
   });
 });
