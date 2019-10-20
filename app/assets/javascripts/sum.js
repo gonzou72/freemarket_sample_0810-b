22359fe383ff983992a299f25611c939afc24809
keisuke imuta <keikeimu12@gmail.com>
2019-10-20 12:25:11 +0900

    update sum.js

--- app/assets/javascripts/sum.js
+++ app/assets/javascripts/sum.js
@@ -4,8 +4,8 @@ $(function () {
     if(val >= 300){
     var fee = val*0.1
     var benefit = val-fee
-    $(".side_fee").text(fee);
-    $(".side_benefit").text(benefit);
+    $(".side_fee").text("¥ " + fee);
+    $(".side_benefit").text("¥ " + benefit);
     }
     if(val<300){
       $(".side_fee").text("-");
