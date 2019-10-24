4d6b99efc86c1391d9ddb8ce38ced87d953dddb3
keisuke imuta <keikeimu12@gmail.com>
2019-10-25 01:44:41 +0900

    update image_display miss

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,11 +1,4 @@
 $(function () {
-  function append(index){
-    var append=`<label for="item_images_attributes_${index+1}_image">
-                      <input type="file" name="item[images_attributes][${index+1}][image]" id="item_images_attributes_${index+1}_image">
-                      <i class="fa fa-camera camera-icon"></i>
-                    </label>`
-    $("#image_field").append(append);
-  };
 
   i=["0","1","2","3","4","5","6","7","8","9"];
 
@@ -13,12 +6,35 @@ $(function () {
     $(document).on("change","#item_images_attributes_" + index + "_image",function() { 
       $("#item_images_attributes_" + index + "_image").css("display","none")
       $(".camera-icon").css("display","none")
-      append(index);
+      var files = $(this).prop('files');
+      var file = files[0];
+      var fr = new FileReader();
+      fr.onload = function(e){
+        var src = e.target.result;
+        var img =`<img src="${src}" id="image_${index}" height=78px width=70px>
+                    <label for="item_images_attributes_${index+1}_image" id="image_field">
+                      <input type="file" name="item[images_attributes][${index+1}][image]" id="item_images_attributes_${index+1}_image">
+                      <i class="fa fa-camera camera-icon"></i>
+                    </label>`;
+        $("#preview").append(img);
+      }
+      fr.readAsDataURL(file);
+      $("#preview").css({
+        "display":"block",
+        "height":"0",
+      });
+      $("#item_images_attributes_1_image").css({
+        "position":"relative",
+      });
       if (index == 9){
         $("#item_images_attributes_10_image").css("display","none")
       }  
     });
   });
+  $(document).on("mouseover","#image_0",function(){
+    console.log("ffff")
+  });  
+
   $(function(){
     for (var i = 0; i < 10; i++) {
       if($("#item_images_attributes_" + i + "_id").length){
